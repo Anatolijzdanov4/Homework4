7457443d233da017a938a1b0d8af1c91e41ddbec
anatolij <anatolijzdanov4@gmail.com>
2023-06-22 20:35:35 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+
+int ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NumberLen(int A)
+{
+    int index = 0;
+    while (A > 0)
+    {
+        A /= 10;
+        index++;
+    }
+    return index;
+}
+
+void SumNumbers(int N, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += N % 10;
+        N /= 10;
+    }
+    Console.WriteLine(sum);
+}
+int number = ReadInt("Введите число: ");
+int len = NumberLen(number);
+SumNumbers(number, len);
